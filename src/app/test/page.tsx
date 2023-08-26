9256eac4744fb17732d4e89fdc55f3d3c3851934
crack-stein <rico47198@live.de>
2023-08-26 23:39:51 +0200

    easy

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl text-red-600 ">xDDDDDDD</h1>
+      <Button zahl={0} />
+    </main>
+  );
+}
