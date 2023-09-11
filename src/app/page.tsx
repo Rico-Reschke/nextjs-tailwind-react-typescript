c1d2978739e17272b7ac6756735c73d19bc9f461
crack-stein <rico47198@live.de>
2023-09-11 15:38:54 +0200

    bugMT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { Navbar } from "@/components/Navbar";
 import React from "react";
 
 export default function HomePage() {
   const [currentUser, setCurrentUser] = React.useState(null);
   return (
-    <div>
-      <Navbar />
-      <div className="flex flex-grow flex-col items-center justify-center text-center">
+    <div className="h-screen flex flex-col justify-between">
+      <div className="items-center justify-center text-center">
         <div className="mx-auto flex h-full w-full max-w-cover flex-col p-4">
           <header className="mb-auto">
             <div>
@@ -37,26 +35,29 @@ export default function HomePage() {
               </nav>
             </div>
           </header>
-            <main className="px-3">
-              <h1>YelpCamp</h1>
-              <p className="text-lg leading-relaxed">
-                Welcome to YelpCamp <br /> Lorem ipsum, dolor sit amet
-                consectetur adipisicing elit. Iure maiores ipsa quia sint
-                suscipit magnam aliquid fuga, doloremque obcaecati nostrum omnis
-                fugit, quas deleniti. Quod laborum libero inventore delectus
-                aperiam?
-              </p>
-              <a
-                href="/campgrounds"
-                className="btn-secondary btn-secondary rounded border border-white bg-white px-4 py-2 text-lg font-bold"
-              >
-                View Campgrounds
-              </a>
-            </main>
-            <footer className="mt-auto text-white opacity-50">
-              <p>&copy; 2020 YelpCamp</p>
-            </footer>
         </div>
+      </div>
+      <div className="">
+        <main className="px-3">
+          <h1>YelpCamp</h1>
+          <p className="">
+            Welcome to YelpCamp <br /> Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Iure maiores ipsa quia sint suscipit magnam
+            aliquid fuga, doloremque obcaecati nostrum omnis fugit, quas
+            deleniti. Quod laborum libero inventore delectus aperiam?
+          </p>
+          <a
+            href="/campgrounds"
+            className="btn-secondary btn-secondary rounded border border-white bg-white px-4 py-2 text-lg font-bold !mt-10"
+          >
+            View Campgrounds
+          </a>
+        </main>
+      </div>
+      <div>
+        <footer className="text-white opacity-50">
+          <p>&copy; 2023 Crackstein</p>
+        </footer>
       </div>
     </div>
   );
