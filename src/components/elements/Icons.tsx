791eef3839355f7b64304e43215cba17870634db
crack-stein <rico47198@live.de>
2023-10-01 02:22:58 +0200

    build 0.1.4

---
+++ src/components/elements/Icons.tsx
@@ -0,0 +1,5 @@
+export { BsSun, BsMoon } from 'react-icons/bs' // Sun icon
+export { BiDesktop, BiPlus } from 'react-icons/Bi' // Desktop icon
+export { MdAppRegistration } from 'react-icons/Md' // AppRegistration icon
+export { RiLoginBoxLine } from 'react-icons/Ri' // Login icon
+export { HiOutlineMap } from 'react-icons/Hi' // XMark icon
--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -1,21 +1,20 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
-import React from "react";
 import { useRouter, usePathname } from "next/navigation";
-import Link from "next/link";
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import {
-  Bars3Icon,
-  XMarkIcon,
-  SunIcon,
-  MoonIcon,
-  ComputerDesktopIcon,
-} from "@heroicons/react/24/outline";
-import { PlusIcon } from "@heroicons/react/20/solid";
-import { RiLoginBoxLine } from "react-icons/ri";
-import { MdAppRegistration } from "react-icons/md";
+  BsMoon,
+  BsSun,
+  BiDesktop,
+  MdAppRegistration,
+  RiLoginBoxLine,
+  BiPlus,
+} from "./icons";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -25,8 +24,7 @@ const Navbar = () => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
-
-  console.log(session);
+  const [icon, setIcon] = useState("ComputerDesktopIcon");
 
   // useEffect(() => {
   //   setPathname(router.pathname);
@@ -42,7 +40,7 @@ const Navbar = () => {
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 justify-between">
               <div className="flex">
-                <div className="lgcustom:hidden -ml-2 mr-2 flex items-center">
+                <div className="-ml-2 mr-2 flex items-center lgcustom:hidden">
                   {/* Mobile menu button */}
                   <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                     <span className="sr-only">Open main menu</span>
@@ -62,7 +60,7 @@ const Navbar = () => {
                     alt="Your Company"
                   />
                 </div>
-                <div className="lgcustom:ml-6 lgcustom:flex lgcustom:space-x-8 hidden">
+                <div className="hidden lgcustom:ml-6 lgcustom:flex lgcustom:space-x-8">
                   <div className="hidden md:ml-6 md:flex md:space-x-8">
                     {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                     <Link
@@ -105,15 +103,13 @@ const Navbar = () => {
                     className="relative mr-5 inline-block text-left"
                   >
                     <div>
-                      {}
                       <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ">
-                        <SunIcon
+                        <BiDesktop
                           className="h-5 w-5 text-gray-400"
                           aria-hidden="true"
                         />
                       </Menu.Button>
                     </div>
-
                     <Transition
                       as={Fragment}
                       enter="transition ease-out duration-100"
@@ -136,7 +132,7 @@ const Navbar = () => {
                                   "group flex items-center px-1 py-1",
                                 )}
                               >
-                                <SunIcon
+                                <BsSun
                                   className="flex h-5 w-5 self-center text-gray-400 group-hover:text-gray-500"
                                   aria-hidden="true"
                                 />
@@ -154,7 +150,7 @@ const Navbar = () => {
                                   "group flex items-center px-1 py-1",
                                 )}
                               >
-                                <MoonIcon
+                                <BsMoon
                                   className="flex h-5 w-5 self-center text-gray-400 group-hover:text-gray-500"
                                   aria-hidden="true"
                                 />
@@ -172,7 +168,7 @@ const Navbar = () => {
                                   "group flex items-center px-1 py-1",
                                 )}
                               >
-                                <ComputerDesktopIcon
+                                <BiDesktop
                                   className="flex h-5 w-5 self-center text-gray-400 group-hover:text-gray-500"
                                   aria-hidden="true"
                                 />
@@ -188,7 +184,7 @@ const Navbar = () => {
                     className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     role="button"
                   >
-                    <PlusIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
+                    <BiPlus className="-ml-0.5 h-5 w-5" aria-hidden="true" />
                     New Campground
                   </a>
                   {!session && (
@@ -220,72 +216,74 @@ const Navbar = () => {
                 </div>
                 <div className="mdlgcustom:ml-4 hidden md:flex md:flex-shrink-0 md:items-center">
                   {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <span className="sr-only">Open user menu</span>
-                        <Image
-                          className="h-8 w-8 rounded-full"
-                          width={40}
-                          height={40}
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700",
-                              )}
-                            >
-                              Your Profile
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700",
-                              )}
-                            >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Link
-                              onClick={() => signOut()}
-                              href="/"
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "block px-4 py-2 text-sm text-gray-700",
-                              )}
-                            >
-                              Sign out
-                            </Link>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
+                  {session && (
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                          <span className="sr-only">Open user menu</span>
+                          <Image
+                            className="h-8 w-8 rounded-full"
+                            width={40}
+                            height={40}
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700",
+                                )}
+                              >
+                                Your Profile
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700",
+                                )}
+                              >
+                                Settings
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Link
+                                onClick={() => signOut()}
+                                href="/"
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700",
+                                )}
+                              >
+                                Sign out
+                              </Link>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  )}
                 </div>
               </div>
             </div>
