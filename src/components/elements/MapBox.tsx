d24a251e5e6c2619b486620838574dd682770c4e
don.cryptus <don.cryptus@gmail.com>
2023-10-01 22:24:18 +0200

    easy

--- src/components/elements/MapBox.tsx
+++ src/components/elements/MapBox.tsx
@@ -2,11 +2,12 @@
 
 import React, { useEffect } from "react";
 import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export default function MapBox() {
   useEffect(() => {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN as any;
-    const map = new mapboxgl.Map({
+    new mapboxgl.Map({
       container: "deine-map-id",
       style: "mapbox://styles/mapbox/streets-v11",
       center: [13.405, 52.52],
@@ -14,7 +15,5 @@ export default function MapBox() {
     });
   }, []);
 
-  return (
-    <div id="deine-map-id" style={{ width: "80%", height: "500px" }}></div>
-  );
+  return <div id="deine-map-id" style={{ width: "80%", height: "500px" }} />;
 }
