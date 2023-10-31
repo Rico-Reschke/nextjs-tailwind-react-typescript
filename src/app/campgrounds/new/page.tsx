17609d26a4965f36fd852e26f8c6a06ccdcf9db5
crack-stein <rico47198@live.de>
2023-10-31 19:53:37 +0100

    dev 0.3.5 backup for multiple image upload

--- src/app/campgrounds/new/page.tsx
+++ src/app/campgrounds/new/page.tsx
@@ -136,6 +136,7 @@ const NewCampgroundForm = () => {
                 <input
                   {...register("imageUrl")}
                   type="file"
+                  multiple
                   aria-describedby="file_input_help"
                   id="file_input"
                   accept="image/*"
