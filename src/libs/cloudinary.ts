0b7e4d6b6fcf5aec6e673d776526e1bb038f47b0
don.cryptus <don.cryptus@gmail.com>
2023-11-14 12:19:52 +0100

    easy

--- src/libs/cloudinary.ts
+++ src/libs/cloudinary.ts
@@ -3,30 +3,17 @@ import { Readable } from "stream";
 
 export const uploadImage = (
   file: File,
-): Promise<UploadApiResponse | undefined> => {
-  return new Promise(async (resolve, reject) => {
-    // Convert the ArrayBuffer to a Readable stream
+): Promise<UploadApiResponse | undefined> =>
+  new Promise(async (resolve, reject) => {
     const buffer = Buffer.from(await file.arrayBuffer());
     const readableStream = new Readable();
     readableStream.push(buffer);
-    readableStream.push(null); // Signal the end of file data
+    readableStream.push(null);
 
-    // Upload the file to Cloudinary
     const uploadStream = cloudinary.uploader.upload_stream(
-      {
-        resource_type: "image",
-        public_id: file.name, // Assuming file.name is 'image.jpg', public_id will be 'image'
-      },
-      (error, result) => {
-        if (error) {
-          console.error(error);
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      },
+      { resource_type: "image", public_id: file.name },
+      (error, result) => (error ? reject(error) : resolve(result)),
     );
 
-    readableStream.pipe(uploadStream); // Pipe the Readable stream to the Cloudinary upload stream
+    readableStream.pipe(uploadStream);
   });
-};
