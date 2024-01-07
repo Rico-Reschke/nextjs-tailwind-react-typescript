cba3609d553833726493680d7180bccd94fac162
crack-stein <rico47198@live.de>
2024-01-07 22:52:52 +0100

    Reviews

--- src/components/utils/ReviewModal.tsx
+++ src/components/utils/ReviewModal.tsx
@@ -2,6 +2,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import React, { Fragment } from "react";
 import { AiFillCloseSquare } from "react-icons/ai";
 import "../../styles/stars.css";
+import Head from "next/head";
 
 interface ModalProps {
   open: boolean;
@@ -9,11 +10,7 @@ interface ModalProps {
   description: string;
 }
 
-export const ReviewModal: React.FC<ModalProps> = ({
-  open,
-  setOpen,
-  description,
-}) => {
+export const ReviewModal: React.FC<ModalProps> = ({ open, setOpen }) => {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={setOpen}>
@@ -51,87 +48,90 @@ export const ReviewModal: React.FC<ModalProps> = ({
                     <AiFillCloseSquare className="h-6 w-6" aria-hidden="true" />
                   </button>
                 </div>
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <Dialog.Description
-                      as="h3"
-                      className="flex items-center justify-center text-base font-semibold leading-6 text-gray-900"
-                    >
-                      Write a Review
-                    </Dialog.Description>
-                    <div className="mt-4">
-                      <textarea
-                        placeholder="Write your review here..."
-                        className="h-32 w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-sm text-gray-700 focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                      ></textarea>
+                <form>
+                  <div className="">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Description
+                        as="h3"
+                        className="flex items-center justify-center text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Write a Review
+                      </Dialog.Description>
+                      <div className="mt-4">
+                        <textarea
+                          placeholder="Write your review here..."
+                          className="h-32 w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-sm text-gray-700 focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                        ></textarea>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <fieldset className="starability-basic">
-                  <input
-                    type="radio"
-                    id="no-rate"
-                    className="input-no-rate"
-                    name="review[rating]"
-                    value="1"
-                    checked
-                    aria-label="No rating."
-                  />
-                  <input
-                    type="radio"
-                    id="first-rate1"
-                    name="review[rating]"
-                    value="1"
-                  />
-                  <label htmlFor="first-rate1" title="Terrible">
-                    1 star
-                  </label>
-                  <input
-                    type="radio"
-                    id="first-rate2"
-                    name="review[rating]"
-                    value="2"
-                  />
-                  <label htmlFor="first-rate2" title="Not good">
-                    2 stars
-                  </label>
-                  <input
-                    type="radio"
-                    id="first-rate3"
-                    name="review[rating]"
-                    value="3"
-                  />
-                  <label htmlFor="first-rate3" title="Average">
-                    3 stars
-                  </label>
-                  <input
-                    type="radio"
-                    id="first-rate4"
-                    name="review[rating]"
-                    value="4"
-                  />
-                  <label htmlFor="first-rate4" title="Very good">
-                    4 stars
-                  </label>
-                  <input
-                    type="radio"
-                    id="first-rate5"
-                    name="review[rating]"
-                    value="5"
-                  />
-                  <label htmlFor="first-rate5" title="Amazing">
-                    5 stars
-                  </label>
-                </fieldset>
-                <div className="mt-5 sm:mt-7 sm:flex sm:flex-row-reverse">
-                  <button
-                    type="button"
-                    className="mr-2 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                  >
-                    Submit
-                  </button>
-                </div>
+                  <div className="flex justify-center">
+                    <fieldset className="starability-basic">
+                      <input
+                        type="radio"
+                        id="no-rate"
+                        className="input-no-rate"
+                        name="review[rating]"
+                        value="1"
+                        aria-label="No rating."
+                      />
+                      <input
+                        type="radio"
+                        id="first-rate1"
+                        name="review[rating]"
+                        value="1"
+                      />
+                      <label htmlFor="first-rate1" title="Terrible">
+                        1 star
+                      </label>
+                      <input
+                        type="radio"
+                        id="first-rate2"
+                        name="review[rating]"
+                        value="2"
+                      />
+                      <label htmlFor="first-rate2" title="Not good">
+                        2 stars
+                      </label>
+                      <input
+                        type="radio"
+                        id="first-rate3"
+                        name="review[rating]"
+                        value="3"
+                      />
+                      <label htmlFor="first-rate3" title="Average">
+                        3 stars
+                      </label>
+                      <input
+                        type="radio"
+                        id="first-rate4"
+                        name="review[rating]"
+                        value="4"
+                      />
+                      <label htmlFor="first-rate4" title="Very good">
+                        4 stars
+                      </label>
+                      <input
+                        type="radio"
+                        id="first-rate5"
+                        name="review[rating]"
+                        value="5"
+                      />
+                      <label htmlFor="first-rate5" title="Amazing">
+                        5 stars
+                      </label>
+                    </fieldset>
+                  </div>
+                  <div className="mt-5 sm:mt-7 sm:flex sm:flex-row-reverse">
+                    <button
+                      type="button"
+                      className="mr-2 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setOpen(false)}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
               </Dialog.Panel>
             </Transition.Child>
           </div>
